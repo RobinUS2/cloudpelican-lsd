1cc1d89dbe202cba1a1d37e125a56bc931053272
Robin Verlangen <robin@us2.nl>
2015-06-07 23:19:42 +0200

    Begin loading filters from supervisor

---
+++ storm/src/main/java/nl/us2/cloudpelican/stormprocessor/Filter.java
@@ -0,0 +1,7 @@
+package nl.us2.cloudpelican.stormprocessor;
+
+/**
+ * Created by robin on 07/06/15.
+ */
+public class Filter {
+}
--- storm/src/main/java/nl/us2/cloudpelican/stormprocessor/MatchBolt.java
+++ storm/src/main/java/nl/us2/cloudpelican/stormprocessor/MatchBolt.java
@@ -3,14 +3,19 @@ package nl.us2.cloudpelican.stormprocessor;
 /**
  * Created by robin on 07/06/15.
  */
+import backtype.storm.Config;
 import backtype.storm.task.OutputCollector;
 import backtype.storm.task.TopologyContext;
 import backtype.storm.topology.OutputFieldsDeclarer;
 import backtype.storm.topology.base.BaseRichBolt;
 //import backtype.storm.tuple.Fields;
 import backtype.storm.tuple.Tuple;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import storm.starter.util.TupleHelpers;
 //import backtype.storm.tuple.Values;
 //import static backtype.storm.utils.Utils.DEFAULT_STREAM_ID;
+import java.util.HashMap;
 import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -23,10 +28,17 @@ public class MatchBolt extends BaseRichBolt {
 
     OutputCollector _collector;
     Pattern pattern;
+    HashMap<String, Filter> filters;
+
+    private static final Logger LOG = LoggerFactory.getLogger(Main.class);
 
     public MatchBolt(String regex) {
         super();
-        pattern = Pattern.compile(regex);
+        if (regex != null) {
+            LOG.info("Compiling local regex " + regex);
+            pattern = Pattern.compile(regex);
+        }
+        filters = null;
     }
 
     public void prepare(Map conf, TopologyContext context, OutputCollector collector) {
@@ -34,12 +46,50 @@ public class MatchBolt extends BaseRichBolt {
     }
 
     public void execute(Tuple tuple) {
-        String msg = tuple.getString(0).trim();
+        if (TupleHelpers.isTickTuple(tuple)) {
+            executeTick();
+        } else {
+            executeTuple(tuple);
+        }
+        _collector.ack(tuple);
+    }
 
-        Matcher m = pattern.matcher(msg);
-        boolean b = m.find();
-        if (b) {
-            System.out.println(msg);
+    public void executeTick() {
+        loadFilters();
+    }
+
+    protected void loadFilters() {
+        if (filters == null) {
+            filters = new HashMap<String, Filter>();
+        }
+        // @Todo
+    }
+
+    protected HashMap<String, Filter> getFilters() {
+        if (filters == null) {
+            loadFilters();
+        }
+        return filters;
+    }
+
+    public Map<String, Object> getComponentConfiguration() {
+        Config conf = new Config();
+        int tickFrequencyInSeconds = 1;
+        conf.put(Config.TOPOLOGY_TICK_TUPLE_FREQ_SECS, tickFrequencyInSeconds);
+        return conf;
+    }
+
+
+    public void executeTuple(Tuple tuple) {
+        String msg = tuple.getString(0).trim();
+        // @todo check filters and output to next bolt
+        getFilters();
+        if (pattern != null) {
+            Matcher m = pattern.matcher(msg);
+            boolean b = m.find();
+            if (b) {
+                System.out.println(msg);
+            }
         }
         //_collector.emit(DEFAULT_STREAM_ID, new Values(customerId, uuid, platform, interactionType, interactionValue, interactionUserDefinedSubtype, campaignId, creativeId, pixelId, eventType));
         _collector.ack(tuple);
