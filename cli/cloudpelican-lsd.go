31b91731e497d274e907aad8e3e3a6fc4a4cb5fe
Robin Verlangen <robin@us2.nl>
2015-06-08 20:25:07 +0200

    Working on CLI

--- cli/cloudpelican-lsd.go
+++ cli/cloudpelican-lsd.go
@@ -11,30 +11,45 @@ import (
 	"log"
 	"os"
 	"os/user"
+	"strings"
 )
 
 var customConfPath string
+var verbose bool
 
 func init() {
 	flag.StringVar(&customConfPath, "c", "", "Path to configuration file (default in your home folder)")
+	flag.BoolVar(&verbose, "v", false, "Verbose, debug mode")
 	flag.Parse()
 }
 
 func main() {
-	log.Println("CloudPelican Log Stream Dump (LSD)")
+	if verbose {
+		log.Println("Starting CloudPelican Log Stream Dump (LSD)")
+	}
 
 	loadConf()
 
 	// Listen for user input
+	startConsole()
+}
+
+func startConsole() {
 	reader := bufio.NewReader(os.Stdin)
+	printConsoleWait()
 	for {
 		input, err := reader.ReadString('\n')
 		if err != nil {
 			fmt.Println("Error: ", err)
 		} else {
-			log.Println(input)
+			fmt.Printf("%s\n", strings.TrimSpace(input))
+			printConsoleWait()
 		}
 	}
+}
+
+func printConsoleWait() {
+	fmt.Printf("cloudpelican> ")
 }
 
 func loadConf() (bool, error) {
@@ -49,15 +64,24 @@ func loadConf() (bool, error) {
 	} else {
 		confPath = customConfPath
 	}
+	if verbose {
+		log.Printf("Reading config from %s", confPath)
+	}
 
 	// Rad
 	conf, confErr := ioutil.ReadFile(confPath)
 	if confErr != nil {
 		// Create file
+		if verbose {
+			log.Printf("Creating new empty config file")
+		}
 		ioutil.WriteFile(confPath, make([]byte, 0), 0600)
+		conf, _ = ioutil.ReadFile(confPath)
 	}
 
 	// Print debug
-	log.Println(string(conf))
+	if verbose {
+		log.Printf("Conf: %s", string(conf))
+	}
 	return true, nil
 }
