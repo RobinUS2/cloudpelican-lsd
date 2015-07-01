4b93daa047fdf924c5b1d86ea66eb36465e0c90f
Robin Verlangen <robin@us2.nl>
2015-07-01 08:39:40 +0200

    Empty stub

---
+++ supervisor/bigquery-client/src/main/java/nl/us2/cloudpelican/bigqueryclient/Main.java
@@ -0,0 +1,10 @@
+package nl.us2.cloudpelican.bigqueryclient;
+
+/**
+ * Created by robin on 01/07/15.
+ */
+public class Main {
+    public static void main(String[] args) throws Exception {
+
+    }
+}
